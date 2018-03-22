03c000ac60c104135e18b24a31d0bf50bc6c521f
Nathan Melenbrink <nathanmelenbrink@users.noreply.github.com>
2018-03-22 17:52:42 -0400

    update week 9

--- 09_media/sketch.js
+++ 09_media/sketch.js
@@ -1,12 +1,26 @@
-let capture;
+var fingers;
 
 function setup() {
-  createCanvas(400, 400);
-  capture = createCapture();
-  capture.hide();
+  createCanvas(320, 240);
+  // specify multiple formats for different browsers
+  fingers = createVideo(['assets/fingers.mov',
+                         'assets/fingers.webm']);
+  fingers.loop();
+  fingers.hide();
+  noStroke();
+  fill(0);
 }
 
 function draw() {
-  image(capture, 0, 0, width, width * capture.height / capture.width);
-  filter(INVERT);
+  background(255);
+  fingers.loadPixels();
+  var stepSize = 32; //round(constrain(mouseX / 8, 6, 32));
+  for (var y=0; y < height; y+=stepSize) {
+    for (var x=0; x < width; x+=stepSize) {
+      var i = y * width + x;
+      var darkness = (255 - fingers.pixels[i*4]) / 255;
+      var radius = stepSize * darkness;
+      ellipse(x, y, radius, radius);
+    }
+  }
 }
